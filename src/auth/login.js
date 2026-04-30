3bb8126f7df54679a34e52d2e4d3f3876a2f29e7
meenalmeenal <meenalpatle4@gmail.com>
2026-04-30 19:52:52 +0530

    Delete src/auth directory

--- src/auth/login.js
+++
@@ -1,9 +0,0 @@
-// Login module
-function login(email, password) {
-    if (!email || !password) {
-        throw new Error('Email and password are required');
-    }
-    return { success: true, user: { email } };
-}
-
-module.exports = { login };
